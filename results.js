a21ee72accf3714eca0edd5a970e7e1c9defa8a4
hectorlavado99 <58657138+hectorlavado99@users.noreply.github.com>
2026-06-20 13:33:01 +0200

    Update results.js

--- results.js
+++ results.js
@@ -17,11 +17,15 @@ const RESULTS = {
   "groupMatches": {
     "A": {
       "Mexico__South Africa": { "home": 2, "away": 0 },
-      "South Korea__Czech Republic": { "home": 2, "away": 1 }
+      "South Korea__Czech Republic": { "home": 2, "away": 1 },
+      "Czech Republic__South Africa": { "home": 1, "away": 1 },
+      "Mexico__South Korea": { "home": 1, "away": 0 }
     },
     "B": {
       "Canada__Bosnia & Herzegovina": { "home": 1, "away": 1 },
-      "Qatar__Switzerland": { "home": 1, "away": 1 }
+      "Qatar__Switzerland": { "home": 1, "away": 1 },
+      "Switzerland__Bosnia & Herzegovina": { "home": 4, "away": 1 },
+      "Canada__Qatar": { "home": 6, "away": 0 }
     },
     "C": {
       "Brazil__Morocco": { "home": 1, "away": 1 },
@@ -39,14 +43,30 @@ const RESULTS = {
       "Netherlands__Japan": { "home": 2, "away": 2 },
       "Sweden__Tunisia": { "home": 5, "away": 1 }
     },
-    "G": {},
-    "H": {
-      "Spain__Cape Verde": { "home": 0, "away": 0 }
+    "G": {
+      "Belgium__Egypt": { "home": 1, "away": 1 },
+      "Iran__New Zealand": { "home": 2, "away": 2 }
     },
-    "I": {},
-    "J": {},
-    "K": {},
-    "L": {}
+    "H": {
+      "Spain__Cape Verde": { "home": 0, "away": 0 },
+      "Saudi Arabia__Uruguay": { "home": 1, "away": 1 }
+    },
+    "I": {
+      "France__Senegal": { "home": 3, "away": 1 },
+      "Iraq__Norway": { "home": 1, "away": 4 }
+    },
+    "J": {
+      "Argentina__Algeria": { "home": 3, "away": 0 },
+      "Austria__Jordan": { "home": 3, "away": 1 }
+    },
+    "K": {
+      "Portugal__DR Congo": { "home": 1, "away": 1 },
+      "Uzbekistan__Colombia": { "home": 1, "away": 3 }
+    },
+    "L": {
+      "England__Croatia": { "home": 4, "away": 2 },
+      "Ghana__Panama": { "home": 1, "away": 0 }
+    }
   },
   "knockout": {
     "round32": [],
